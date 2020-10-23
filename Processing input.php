bf072fdef8c72b55d4d33e39672edb2336553a4a
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-10-23 15:30:01 +0530

    Processing input of user

---
+++ Processing input.php
@@ -0,0 +1,7 @@
+<?php
+if(!empty($_POST["submit"])){
+	echo "the username of user is".$_POST["username"];
+	echo "<br/>";
+	echo "the password of user is".$_POST["password"];
+}
+?>
---
+++ sending data using input.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<form action="./Processing input.php" method='post'>
+Username:<input type="text" value="" name="username" />
+Password:<input type="text" value="" name="password" />
+<input type="submit" value="submit" name="submit" />
+</form>
