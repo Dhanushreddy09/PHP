9049303f888e05800d4084df4c2f1830534c7b67
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-10-27 22:34:49 +0530

    added automatic file uploads

--- Processing files.php
+++ Processing files.php
@@ -1,7 +1,26 @@
 <?php
 if(!empty($_POST['submit'])){
 	if($_FILES['upload']['error']==0){
-		if(move_uploaded_file($_FILES['upload']['tmp_name'],"./wampthemes/".$_FILES['upload']['name'])){
+		switch($_FILES['upload']['type']){
+			case"image/jpeg":
+			echo "That was a valid file type";
+			break;
+			default:
+			exit("invalid file type");
+		}
+		$array=$_FILES['upload']['name'];
+		$extension=explode(".",$array);
+		$file_name_extension=array_pop($extension);
+		$new_file_name=time().rand(1000,9999).".".$file_name_extension;
+		$new_file_destination="./".date("Y")."/".date("m")."/".date("d");
+		if(!is_dir($new_file_destination)){
+		 mkdir($new_file_destination,0777,true);
+		 $destination=$new_file_destination."/".$new_file_name;
+		}else{
+			// directory already exists
+			$destination=$new_file_destination."/".$new_file_name;
+		}
+		if(move_uploaded_file($_FILES['upload']['tmp_name'],$destination)){
 			echo "File succesfully uploaded";
 		}
 		else{
