51b60acb4e97266aa836067bacf051bc1936b66a
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-11-04 22:04:48 +0530

    Updated insertion to take input from user

--- insert.php
+++ insert.php
@@ -1,12 +1,28 @@
 <?php
 $posttime=date('Y-m-d H:i:s');
 require_once("./connect.php");
-$sql="INSERT INTO `message` SET `user_name`='hemsworth',`post_time`='{$posttime}',`message`='I almost learnt curd'";
+/*$sql="INSERT INTO `message` SET `user_name`='hemsworth',`post_time`='{$posttime}',`message`='I almost learnt curd'";
 $db->query($sql);
 if($db->error){
 	echo $db->error;
 }
 else{
 	echo "row added successfully";
+}*/
+if(!empty($_POST['submit'])){
+	$sql="INSERT INTO `message` SET `user_name`='{$_POST['user_name']}',`post_time`='{$posttime}',`message`='{$_POST['message']}'";
+	$db->query($sql);
+	if($db->error){
+	echo $db->error;
+}
+else{
+	echo "<br /> row added successfully";
+}
 }
 ?>
+<form action="" method="POST">
+<br />
+User Name:<input type="text" name="user_name" value ="" /><br />
+Enter Message:<input type="text" name="message" value="" /><br />
+<input type="submit" value="submit" name="submit" />
+</form>
