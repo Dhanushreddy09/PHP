fd7a6c194c5a5c184cec70adf2e0f7fa9154bf61
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-12-07 21:29:56 +0530

    DOCS

--- DOCS/arrays.php
+++ DOCS/arrays.php
@@ -11,5 +11,27 @@ $array[]=56;
 print_r($array);
 //to reindex the array keys array values built in  function is used
 $array=array_values($array);
+print_r($array);
+//array dereferencing
+function getarray(){
+    return array(1,2,3,56);
+}
+$firstelement=getarray()[0];
+
+echo $firstelement;
+
+/*string literal keys(have to use single quotes while using string literal as key) and
+ This does not mean to always quote the key. Do not quote keys which are 
+constants or variables, as this will prevent PHP from interpreting them.*/
+$array['bar']='foo';
+print_r($array);
+
+//count
+echo count($array);
+
+//copying arrays by reference
+
+$array2=&$array;
+$array2[]='Reference';
 print_r($array)
 ?>
